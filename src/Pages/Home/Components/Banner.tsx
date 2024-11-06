d31f4212ece8eff1e33ad6248c24344d967eec5f
konika098 <konikakhan2000@gmail.com>
2024-11-06 15:12:50 +0600

    fixed slide issues

--- src/Pages/Home/Components/Banner.tsx
+++ src/Pages/Home/Components/Banner.tsx
@@ -1,19 +1,39 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const Banner = () => {
+  // Slider settings
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className="hero_section main_section">
       <div className="container">
         <div className="hero-area">
           <div className="color"></div>
-          <div className="row" style={{paddingTop:"110px"}}>
+          <div className="row" style={{ paddingTop: "110px" }}>
             <div className="col-lg-6">
               <h2 className="hero-title">
-                <img className="img-fluid" src="https://i.postimg.cc/FsGL06Tn/lamp.png" alt="img" />
+                <img
+                  className="img-fluid"
+                  src="https://i.postimg.cc/FsGL06Tn/lamp.png"
+                  alt="img"
+                />
                 Discovering the Best
                 <br className="d-none d-md-block d-lg-none" />
                 Furniture for Your Home
-                <img className="img-fluid" src="https://i.postimg.cc/FHBkBL5y/bed.png" alt="img" />
+                <img
+                  className="img-fluid"
+                  src="https://i.postimg.cc/FHBkBL5y/bed.png"
+                  alt="img"
+                />
               </h2>
               <p className="hero-text">
                 Embrace the beauty of minimalism in your home.
@@ -31,7 +51,10 @@ const Banner = () => {
                 </a>
                 <a href="#">
                   <button className="button btn_light">
-                    <i className="fa-regular fa-circle-play" style={{marginRight:"4px"}}/>
+                    <i
+                      className="fa-regular fa-circle-play"
+                      style={{ marginRight: "4px" }}
+                    />
                     Watch Video
                   </button>
                 </a>
@@ -40,34 +63,36 @@ const Banner = () => {
             </div>
             <div className="col-lg-6">
               <div className="hero-slider-section">
-                <div className="content">
-                  <img
-                    className="img-fluid"
-                    src="https://i.postimg.cc/rFhDDrpB/hero-sofa.png"
-                    alt="img"
-                  />
-                </div>
-                <div className="content">
-                  <img
-                    className="img-fluid"
-                    src="https://i.postimg.cc/rFhDDrpB/hero-sofa.png"
-                    alt="img"
-                  />
-                </div>
-                <div className="content">
-                  <img
-                    className="img-fluid"
-                    src="https://i.postimg.cc/rFhDDrpB/hero-sofa.png"
-                    alt="img"
-                  />
-                </div>
-                <div className="content">
-                  <img
-                    className="img-fluid"
-                    src="https://i.postimg.cc/rFhDDrpB/hero-sofa.png"
-                    alt="img"
-                  />
-                </div>
+                <Slider {...settings}>
+                  <div className="content">
+                    <img
+                      className="img-fluid"
+                      src="https://i.postimg.cc/rFhDDrpB/hero-sofa.png"
+                      alt="img"
+                    />
+                  </div>
+                  <div className="content">
+                    <img
+                      className="img-fluid"
+                      src="https://i.postimg.cc/rFhDDrpB/hero-sofa.png"
+                      alt="img"
+                    />
+                  </div>
+                  <div className="content">
+                    <img
+                      className="img-fluid"
+                      src="https://i.postimg.cc/rFhDDrpB/hero-sofa.png"
+                      alt="img"
+                    />
+                  </div>
+                  <div className="content">
+                    <img
+                      className="img-fluid"
+                      src="https://i.postimg.cc/rFhDDrpB/hero-sofa.png"
+                      alt="img"
+                    />
+                  </div>
+                </Slider>
               </div>
             </div>
           </div>
